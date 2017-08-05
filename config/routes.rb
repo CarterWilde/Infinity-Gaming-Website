38aa4230c23f7b59e3d7ffb4df2e7ac928b6c238
CarterWilde <carterjwilde@gmail.com>
2017-08-05 01:36:22 -0600

    Added games
    TODO:Scaling
    TODO: Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-    get 'index', to: 'pages#index'
-    get 'games', to: 'pages#games'
-    root to: 'pages#index'
+    get 'index' => 'pages#index'
+    get 'about' => 'pages#about'
+    get 'contact' => 'pages#contact'
+    get 'images' => 'pages#images'
+    get 'videos' => 'pages#videos'
+    get 'games' => 'pages#games'
+
+    root 'pages#index'
 end
